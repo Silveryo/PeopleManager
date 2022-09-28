a45ed407990efdec5bbd11c13abea3926713cfc3
Zdenek <silverdaboss@gmail.com>
2022-09-28 12:16:23 +0200

    Added people controller and index view

---
+++ PeopleManager.Ui.WebApp/Controllers/PeopleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PeopleManager.Ui.WebApp.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
